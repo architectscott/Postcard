56e4d63d00f7ca9fe2f66bfe58cab5f235474cb9
Scott Simmons <scottsimmons@Scotts-MacBook-Pro.local>
2014-12-21 14:12:46 -0700

    Added second label for name

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mailButton: UIButton!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -36,10 +38,17 @@ class ViewController: UIViewController {
       messageLabel.text = enterMessageTextField.text
       messageLabel.textColor = UIColor.redColor()
         
+      nameLabel.hidden = false
+      nameLabel.text = enterNameTextField.text
+      nameLabel.textColor = UIColor.blueColor()
+        
       enterMessageTextField.text = ""
       enterMessageTextField.resignFirstResponder()
         
-      mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    enterNameTextField.text = ""
+    enterNameTextField.resignFirstResponder()
+      
+    mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
     
 
